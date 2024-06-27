22f7704e9741839f05aaaeacad98e58e309f9f8c
gouresh <goureshghadi40@gmail.com>
2024-06-27 16:59:52 +0530

    initial commit

---
+++ src/main/kotlin/com/gouresh/employeerestapidemo/EmployeeRestApiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.gouresh.employeerestapidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeRestApiDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmployeeRestApiDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/gouresh/employeerestapidemo/EmployeeRestApiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gouresh.employeerestapidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployeeRestApiDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
