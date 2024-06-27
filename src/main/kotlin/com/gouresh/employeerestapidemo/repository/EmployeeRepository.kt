6e225817d93266683dc39c8810bacdf9e7e933fa
gouresh <goureshghadi40@gmail.com>
2024-06-27 20:10:42 +0530

    Implemented Spring data rest

--- src/main/kotlin/com/gouresh/employeerestapidemo/repository/EmployeeRepository.kt
+++ src/main/kotlin/com/gouresh/employeerestapidemo/repository/EmployeeRepository.kt
@@ -2,6 +2,8 @@ package com.gouresh.employeerestapidemo.repository
 
 import com.gouresh.employeerestapidemo.entity.Employee
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
+@RepositoryRestResource(path = "members")
 interface EmployeeRepository : JpaRepository<Employee,Int> {
 }
